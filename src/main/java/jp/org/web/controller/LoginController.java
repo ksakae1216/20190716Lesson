1cadba63c88a90219cadeb52271766a264ae7582
kotaro_sakae <ksakae1216@gmail.com>
2019-07-16 21:22:56 +0900

    password change

--- src/main/java/jp/org/web/controller/LoginController.java
+++ src/main/java/jp/org/web/controller/LoginController.java
@@ -43,7 +43,7 @@ public class LoginController {
 		logger.info("Login - doLogin start");
 
 		String ret = "login";
-		if(loginForm.getLoginId().equals("abc") && loginForm.getPassword().equals("def")) {
+		if(loginForm.getLoginId().equals("abc") && loginForm.getPassword().equals("zzz")) {
 			ret = "home";
 		}else {
 		logger.info("Login - doLogin stop");
